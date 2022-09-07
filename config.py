1f7c4b23d2c7dcdd398773190bc2965b98753d66
HongYuChanTT <93893776+HongYuChanTT@users.noreply.github.com>
2022-09-07 17:18:37 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "database-1.cvoosl9bnaez.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
-customdb = "database-1"
+customdb = "employee"
 custombucket = "chanhongyu-employee"
 customregion = "us-east-1"
